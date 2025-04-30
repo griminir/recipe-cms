f05a94602fec37bb634885cb3a9d89a341b24099
griminir <viktor.elliot.king@gmail.com>
2025-04-30 10:56:11 +0200

    added validation to category

--- schemaTypes/category.ts
+++ schemaTypes/category.ts
@@ -9,11 +9,17 @@ export default defineType({
       name: 'title',
       title: 'Title',
       type: 'string',
+      validation: rule => rule.required().error('Needed to know what it is')
     }),
     defineField({
       name: 'description',
       title: 'Description',
       type: 'text',
+      hidden: context => context?.document?.title === undefined,
+      validation: rule => rule.custom(description => {
+        if (!description) return "This is not required but website will look better with it"
+        return true
+      }).warning()
     }),
   ],
 })
